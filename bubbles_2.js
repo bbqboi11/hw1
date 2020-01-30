a85281949d5e650e6bedcd3b5108dd2158d2bfe5
bbqboi11 <60369527+bbqboi11@users.noreply.github.com>
2020-01-30 10:45:49 -0800

    Medium (small circle)

--- bubbles_2.js
+++ bubbles_2.js
@@ -1,1 +1,29 @@
+var x;
+var y;
+var radius;
 
+function setup() {
+  createCanvas(1080, 720);
+  x = random(width);
+  y = random(height);
+  radius = 15;
+}
+
+function draw() {
+  background(0);
+
+  if (dist(mouseX, mouseY, x, y) < radius) {
+    if (mouseIsPressed) {
+      x = random(width);
+      y = random(height);
+    }
+    fill(0, 0, 0, 200);
+  }
+  else {
+    fill(255, 255, 255, 255);
+  }
+
+  ellipse(x, y, radius * 2);
+  x += random(-1, 1);
+  y += random(-1, 1);
+}
