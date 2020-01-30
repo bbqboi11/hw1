28254f171296dcacae70ad92eda7296d5a095aba
bbqboi11 <60369527+bbqboi11@users.noreply.github.com>
2020-01-30 10:50:16 -0800

    less shaking more radius

--- bubbles_3.js
+++ bubbles_3.js
@@ -1,1 +1,28 @@
+var x;
+var y;
+var radius;
 
+function setup() {
+  createCanvas(1080, 720);
+  x = random(width);
+  y = random(height);
+  radius = 25;
+}
+
+function draw() {
+  background(0);
+
+  if (dist(mouseX, mouseY, x, y) < radius) {
+    if (mouseIsPressed) {
+      x = random(width);
+      y = random(height);
+    }
+    fill(127, 255, 200, 200);
+  }
+  else {
+    fill(255, 255, 255, 255);
+  }
+
+  ellipse(x, y, radius * 2);
+ 
+}
